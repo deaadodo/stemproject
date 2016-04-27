47adf0708f32b7ca585f0f3c8a8bda52777485b6
sanaa <engsanaa2016@gmail.com>
2016-04-27 17:49:07 +0200

    add english_lessons

---
+++ english_lessons.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+<style>
+body {
+
+	background-color: #E4F0FE;
+}
+
+.style28 {
+	font-size: 30px;
+	 font-weight: bold;
+	  color: #F01E00;
+	   text-align:center;
+  background-color:#FC6;
+  padding:5px 5px;
+  border-radius: 5em;
+  margin-bottom:20px;
+  margin-top:20px;
+	    }
+.style46 {
+	color:#003;
+	font-family: "Comic Sans MS";
+	font-size: 25px;
+	background-color:#00A8CD;
+  padding:5px 5px;
+  border-radius: 5em;
+ margin-bottom:10px;
+  margin-top:10px;
+   text-align:center;
+  
+	}
+</style>
+</head>
+<body>
+	<div class="row  ">
+    	<div class="col-md-6 col-xs-offset-3 style28">
+    	الوحدة الاولي في الانجليزي
+        </div>
+     </div>
+	<div class="row ">
+    	
+		<div class="col-md-6 col-xs-offset-3 style46 ss">
+        	التهيئة
+        </div>
+	</div>
+    <div class="row">
+		
+		<div class="col-md-6 col-xs-offset-3 style46 ss">
+        	التهيئة
+        </div>
+	</div>
+    
+    <div class="row ">
+    	
+		<div class="col-md-6 col-xs-offset-3 style46 ss">
+        	التهيئة
+        </div>
+	</div>
+    
+   <div class="row ">
+    	
+		<div class="col-md-6 col-xs-offset-3 style46 ss">
+        	التهيئة
+        </div>
+	</div>
+    
+    <div class="row ">
+    	
+		<div class="col-md-6 col-xs-offset-3 style46 ss">
+        	التهيئة
+        </div>
+	</div>
+    <div class="row ">
+    	
+		<div class="col-md-6 col-xs-offset-3 style46 ss">
+        	التهيئة
+        </div>
+	</div>
+    <div class="row ">
+    	
+		<div class="col-md-6 col-xs-offset-3 style46 ss">
+        	التهيئة
+        </div>
+	</div>
+    
+    <div class="row ">
+    	
+		<div class="col-md-6 col-xs-offset-3 style46 ss">
+        	التهيئة
+        </div>
+	</div>
+    <div class="row ">
+    	
+		<div class="col-md-6 col-xs-offset-3 style46 ss">
+        	التهيئة
+        </div>
+	</div>
+   <div class="row ">
+    	
+		<div class="col-md-6 col-xs-offset-3 style46 ss">
+        	التهيئة
+        </div>
+	</div>
+    <div class="row ">
+    	
+		<div class="col-md-6 col-xs-offset-3 style46 ss">
+        	التهيئة
+        </div>
+	</div>
+    <div class="row ">
+    	
+		<div class="col-md-6 col-xs-offset-3 style46 ss">
+        	التهيئة
+        </div>
+	</div>
+    <div class="row ">
+    	
+		<div class="col-md-6 col-xs-offset-3 style46 ss">
+        	التهيئة
+        </div>
+	</div>
+</div>
+</body>
+</html>
