795868064a026b5704f3c7a544abfb0695382440
sanaa <engsanaa2016@gmail.com>
2016-04-27 14:34:16 +0200

    add second_class and third_class

---
+++ second_class.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<style>
+body{
+	background-color:pink;
+	
+	}
+img{padding-top:100px;
+	margin-left:50px;
+	}
+
+h1{
+	text-align:center;
+	color:green;
+	padding-top:50px;
+	}
+
+
+
+
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<h1>الصف الثاني الاعدادي</h1>
+<a href="science_second.php"><img src="img/s.jpg" ></a>
+<a href="english_second.php"><img src="img/e.jpg" ></a>
+<a href="math_second.php"><img src="img/m.jpg" ></a>
+
+
+</body>
+</html>
