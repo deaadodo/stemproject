86cfa4d76305956c71cbdc58cd43dcb0d585f515
sanaa <engsanaa2016@gmail.com>
2016-04-27 14:37:31 +0200

    add science_second and science_third

---
+++ science_second.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+
+<style>
+video{
+	
+	width:45%;
+	height:45%;
+	}
+</style>
+</head>
+<body>
+<div class="row">
+	<div class="col-sm-12">
+		العلوم  للصف الثاني الاعدادي    
+    </div>
+</div>
+<br/>
+<div class="row">
+		<div class="col-sm-6">
+			<p>الفصل الدراسي الثاني</p>
+		</div>
+        <div class="col-sm-6">
+			الفصل الدراسي الاول
+		</div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس الاول</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس الاول</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/الترم الثانى للصف الثانى.علوم/الحركة الاهتزازية - علوم - للصف الثاني الإعدادي - ج1 - موقع نفهم - موقع نفهم - 10Youtube.com.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/الترم الاول للصف الثانى.علوم/محاولات تصنيف العناصر - مندليف وموزلي - علوم - للصف الثاني الإعدادي - موقع نفهم - 10Youtube.com.mp4" controls="controls">
+        </video>
+    </div>
+</div>    
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس الثاني</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس الثاني</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/الترم الثانى للصف الثانى.علوم/الحركة الموجية - علوم - للصف الأول الاعدادي - موقع نفهم - 10Youtube.com.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/تدرج خواص العناصر في الجدول الدوري  -  الخاصية الفلزية واللافلزية  -  علوم للصف الثاني الإعدادي - 10Youtube.com.mp4" controls="controls">
+        </video>
+    </div>
+</div> 
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس الثالث</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس الثالث</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/خصائص الموجات الصوتية (علوم) - 10Youtube.com.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/المجموعات الرئيسية بالجدول الدوري - علوم - للصف الثاني الإعدادي - ج1 - موقع نفهم - موقع نفهم - 10Youtube.com.mp4" controls="controls">
+        </video>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس الرابع</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس الرابع</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/انعكاس الموجات الصوتية - 10Youtube.com.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/شرح درس الماء - مادة العلوم  -  للصف الثاني الإعدادي  - ج1 - موقع نفهم - 10Youtube.com.mp4" controls="controls">
+        </video>
+    </div>
+</div>       
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس الخامس</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس الخامس</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/الطبيعة الموجية للضوء - علوم - الصف الثاني الإعدادي -ج2 - موقع نفهم - 10Youtube.com.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/طبقات الغلاف الجوى - علوم - للصف الثالني الإعدادي - ج2 - موقع نفهم - موقع نفهم - 10Youtube.com.mp4" controls="controls">
+        </video>
+    </div>
+</div>       
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس السادس</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس السادس</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/انعكاس وانكسار الضوء - درس انعكاس و انكسار الضوء - علوم ثانية اعدادي - 10Youtube.com.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/تآكل طبقة الأوزون وارتفاع درجة الحرارة - علوم - للصف الثاني الإعدادي - ج2 - موقع نفهم - 10Youtube.com.mp4" controls="controls">
+        </video>
+    </div>
+</div>              
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس السابع</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس السابع</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/التكاثر فى النبات  علوم - للصف الثاني الإعدادي - موقع نفهم - موقع نفهم - 10Youtube.com (1).mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/الحفريات - 10Youtube.com.mp4" controls="controls">
+        </video>
+    </div>
+</div>       
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس الثامن</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس الثامن</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/01-التكاثر فى الإنسان - 10Youtube.com.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/الإنقراض - علوم - للصف الثاني الإعدادي - ج2 - موقع نفهم - موقع نفهم - 10Youtube.com.mp4" controls="controls">
+        </video>
+    </div>
+</div>       
+
+
+</body>
+</html>
---
+++ science_third.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+
+<style>
+video{
+	
+	width:45%;
+	height:45%;
+	}
+</style>
+</head>
+<body>
+<div class="row">
+	<div class="col-sm-12">
+		العلوم  للصف الثالث الاعدادي    
+    </div>
+</div>
+<br/>
+<div class="row">
+		<div class="col-sm-6">
+			<p>الفصل الدراسي الثاني</p>
+		</div>
+        <div class="col-sm-6">
+			الفصل الدراسي الاول
+		</div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس الاول</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس الاول</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/الترم الثانى للصف الثالث.علوم/التفاعلات الكيميائية - علوم للصف الثالث الاعدادى - 1 - موقع نفهم.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/الترم الاول للصف الثالث.علوم/علوم للصف الثالث الإعدادى _ الحركة فى اتجاه واحد 13-09-2014.mp4" controls="controls">
+        </video>
+    </div>
+</div>    
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس الثاني</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس الثاني</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/الترم الثانى للصف الثالث.علوم/سرعة التفاعلات الكيميائية - علوم للصف الثالث الإعدادي - موقع نفهم.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/الترم الاول للصف الثالث.علوم/التمثيل البيانى للحركة في خط مستقيم - العلوم - للصف الثالث الإعدادي - ج1 - موقع نفهم - موقع نفهم.mp4" controls="controls">
+        </video>
+    </div>
+</div> 
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس الثالث</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس الثالث</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/الترم الثانى للصف الثالث.علوم/المخاليط - علوم - للصف الثالث الاعدادي - موقع نفهم.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/الترم الاول للصف الثالث.علوم/الكميات الفيزيائية القياسية والمتجهة - علوم - للصف الثالث الإعدادي - الجزء الأول - موقع نفهم.mp4" controls="controls">
+        </video>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس الرابع</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس الرابع</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/الترم الثانى للصف الثالث.علوم/الخصائص الفيزيائية للتيار الكهربى علوم 3 ع ترم 2 أ _ أحمد حمدى.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/الترم الاول للصف الثالث.علوم/علوم للصف الثالث الاعدادى -- الوحدة الثانية المرايا.mp4" controls="controls">
+        </video>
+    </div>
+</div>       
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس الخامس</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس الخامس</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/الترم الثانى للصف الثالث.علوم/العلوم للصف الثالث الإعدادي _ أكاديمية الدارين _ المحاضرة 8 - 1_ التيار الكهربى و الاعمدة الكهربية.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/الترم الاول للصف الثالث.علوم/علوم إعدادية - العدسات - 01.mp4" controls="controls">
+        </video>
+    </div>
+</div>       
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس السادس</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس السادس</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/الترم الثانى للصف الثالث.علوم/النشاط الاشعاعى والطاقة النووية - علوم - للصف الثالث الإعدادي - موقع نفهم.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/الترم الاول للصف الثالث.علوم/درس الكون للأستاذ عادل فؤاد.mp4" controls="controls">
+        </video>
+    </div>
+</div>              
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس السابع</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس السابع</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/الترم الثانى للصف الثالث.علوم/المبادئ الأساسية للوراثة - علوم - للصف الثالث الاعدادي - موقع نفهم.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/الترم الاول للصف الثالث.علوم/النظام الشمسى للأستاذ حمدى راغب.mp4" controls="controls">
+        </video>
+    </div>
+</div>       
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس الثامن</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس الثامن</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/الترم الثانى للصف الثالث.علوم/الجينات - علوم - الصف الثالث الإعدادي - موقع نفهم.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/الترم الاول للصف الثالث.علوم/العلوم _ الانقسام الخلوى.mp4" controls="controls">
+        </video>
+    </div>
+</div>       
+<div class="row">
+	<div class="col-sm-6">
+		<p>الدرس التاسع</p>
+    </div>
+    <div class="col-sm-6">
+		<p>الدرس التاسع</p>
+    </div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+    	<video src="video/الترم الثانى للصف الثالث.علوم/التنظيم الهرموني في الإنسان - العلوم - الصف الثالث الإعدادي - موقع نفهم.mp4" controls="controls">
+        </video>
+        </div>
+        <div class="col-sm-6">
+    	<video src="video/الترم الاول للصف الثالث.علوم/التكاثر اللا جنسي والجنسي - علوم - للصف الثالث الإعدادي - ترم أول - موقع نفهم - 10Youtube.com.mp4" controls="controls">
+        </video>
+    </div>
+</div>       
+
+
+</body>
+</html>
