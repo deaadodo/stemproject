89ee9ed9cc60ffc2b2fc92c9eee95ce30b736c5e
sanaa <engsanaa2016@gmail.com>
2016-04-27 14:26:00 +0200

    add course.php and course.css

---
+++ courses.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link rel="stylesheet" type="text/css" href="css/courses_style.css"/>
+<script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div>
+<h1>الصفوف الدراسية</h1>
+</div>
+<div class="content-page">
+<div class="box">
+<img src="img/2.jpg" >
+<div class="title">
+<a href="second_class.php" >الصف الثاني الاعدادي</a>
+</div>
+<!--<div class="clear">
+</div>-->
+</div>
+</div>
+<div class="box">
+<img src="img/3.jpg">
+<div class="title">
+<a href="third_class.php">الصف الثالث الاعدادي</a>
+</div>
+<!--<div class="clear">
+</div>-->
+</div>
+</div>
+</body>
+</html>
