51626a853f6526289aedfb11e28d15d3cde3c431
sanaa <engsanaa2016@gmail.com>
2016-04-27 14:43:27 +0200

    add amalstem folder

---
+++ amalstem/about us.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+
+		
+<link rel="stylesheet" href="css/bootstrap.min.css">
+       
+        <link rel="stylesheet" href="css/templatemo_style.css">
+</head>
+<style>
+p{color:#C36;
+font-size:60px;
+}
+
+
+
+
+</style>
+
+<body>
+  <section id="templatemo_features">
+            <div class="container-fluid">
+                <header class="template_header">
+                    <h1 class="text-center"><span>...</span> من نحن <span>...</span></h1>
+                </header>
+                <div class="col-sm-12">
+                    <div class="col-sm-6 col-lg-3 feature-box">
+                        <div class="feature-box-inner">
+                            <div class="feature-box-icon">
+                           
+                            </div>
+                            <p>
+                                *Luxor STEM School *<br />
+                                الاقصرالثانويه المشتركة فى العلوم والتكنولوجيا
+
+                            
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3 feature-box">
+                        <div class="feature-box-inner">
+                            <div class="feature-box-icon">
+                           
+                            </div>
+                            <p> *stem*<br />
+ من خلال التسجيل لاولياء الامورستمكن الاهل من تلقى الرسائل الخاصة بنا لدعوتهم للحضور الى الندوات التى ستعدها  الاماكن الخاصة بالتدريب 
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3 feature-box">
+                        <div class="feature-box-inner">
+                            <div class="feature-box-icon">
+                        
+                            </div>
+                            <p>
+                             *كيفية التعامل مع التطبيق*<br />
+من خلال التسجيل معنا فى التطبيق سيمكنك ذلك من الدخول على الصفحة الخاصة بك والتى سيخصصها لك التطبيق وسيمكنك من رؤية المواد الخاصةبك
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3 feature-box">
+                        <div class="feature-box-inner">
+                            <div class="feature-box-icon">
+                            
+                            </div>
+                            <p>
+                               *stem تطبيق*<br />
+يوفر لك امكانية   استذكار موادك العلمية الاساسية  كالعلوم والرياضيات والانجليزية ويوفر لك اجراء اختبارات  بعد كل وحدة دراسية
+                               
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+         <script src="js/jquery.min.js"></script>
+        <script src="js/jquery-ui.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.parallax.js"></script>
+       
+        <script src="js/templatemo_scripts.js"></script>
+</body>
+</html>
